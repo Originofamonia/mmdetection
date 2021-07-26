5d59f593e621d6eda17f58313f481f4b10b702e6
Edward <anqiyuanms@hotmail.com>
2021-07-26 11:59:19 -0500

    default='/home/qiyuan/2021summer/mmdetection/configs/yolo/yolov3_d53_320_273e_coco.py',

--- tools/train.py
+++ tools/train.py
@@ -20,27 +20,31 @@ from mmdet.utils import collect_env, get_root_logger
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
-    parser.add_argument('config', help='train config file path')
-    parser.add_argument('--work-dir', help='the dir to save logs and models')
-    parser.add_argument(
-        '--resume-from', help='the checkpoint file to resume from')
+    parser.add_argument('config',
+                        default='/home/qiyuan/2021summer/mmdetection/configs/yolo/yolov3_d53_320_273e_coco.py',
+                        help='train config file path')
+    parser.add_argument('--work-dir',
+                        default='/home/qiyuan/2021summer/mmdetection/work',
+                        help='the dir to save logs and models')
+    parser.add_argument('--resume-from',
+                        default='/home/qiyuan/2021summer/mmdetection/work',
+                        help='the checkpoint file to resume from')
     parser.add_argument(
         '--no-validate',
         action='store_true',
         help='whether not to evaluate the checkpoint during training')
     group_gpus = parser.add_mutually_exclusive_group()
-    group_gpus.add_argument(
-        '--gpus',
-        type=int,
-        help='number of gpus to use '
-        '(only applicable to non-distributed training)')
-    group_gpus.add_argument(
-        '--gpu-ids',
-        type=int,
-        nargs='+',
-        help='ids of gpus to use '
-        '(only applicable to non-distributed training)')
-    parser.add_argument('--seed', type=int, default=None, help='random seed')
+    group_gpus.add_argument('--gpus',
+                            default=2,
+                            type=int,
+                            help='number of gpus to use '
+                                 '(only applicable to non-distributed training)')
+    group_gpus.add_argument('--gpu-ids',
+                            type=int,
+                            nargs='+',
+                            help='ids of gpus to use '
+                                 '(only applicable to non-distributed training)')
+    parser.add_argument('--seed', type=int, default=444, help='random seed')
     parser.add_argument(
         '--deterministic',
         action='store_true',
@@ -50,23 +54,21 @@ def parse_args():
         nargs='+',
         action=DictAction,
         help='override some settings in the used config, the key-value pair '
-        'in xxx=yyy format will be merged into config file (deprecate), '
-        'change to --cfg-options instead.')
-    parser.add_argument(
-        '--cfg-options',
-        nargs='+',
-        action=DictAction,
-        help='override some settings in the used config, the key-value pair '
-        'in xxx=yyy format will be merged into config file. If the value to '
-        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
-        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
-        'Note that the quotation marks are necessary and that no white space '
-        'is allowed.')
-    parser.add_argument(
-        '--launcher',
-        choices=['none', 'pytorch', 'slurm', 'mpi'],
-        default='none',
-        help='job launcher')
+             'in xxx=yyy format will be merged into config file (deprecate), '
+             'change to --cfg-options instead.')
+    parser.add_argument('--cfg-options',
+                        nargs='+',
+                        action=DictAction,
+                        help='override some settings in the used config, the key-value pair '
+                             'in xxx=yyy format will be merged into config file. If the value to '
+                             'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
+                             'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
+                             'Note that the quotation marks are necessary and that no white space '
+                             'is allowed.')
+    parser.add_argument('--launcher',
+                        choices=['none', 'pytorch', 'slurm', 'mpi'],
+                        default='none',
+                        help='job launcher')
     parser.add_argument('--local_rank', type=int, default=0)
     args = parser.parse_args()
     if 'LOCAL_RANK' not in os.environ:

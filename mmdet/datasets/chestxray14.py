0dafe8176d2f402bf7435cf73e0522df603b581c
Edward <anqiyuanms@hotmail.com>
2021-07-27 11:49:26 -0500

    f = open(ann_file)         self.json_data = json.load(f)

--- mmdet/datasets/chestxray14.py
+++ mmdet/datasets/chestxray14.py
@@ -38,6 +38,8 @@ class Chestxray14Dataset(CustomDataset):
                  filter_empty_gt=True):
         super().__init__(ann_file, pipeline, classes, data_root, img_prefix,
                          seg_prefix, proposal_file, test_mode, filter_empty_gt)
+        f = open(ann_file)
+        self.json_data = json.load(f)
         self.cat_ids = list(range(1, len(self.CLASSES)+1))
         self.cat2label = {cat_id: i for i, cat_id in enumerate(self.cat_ids)}
 
@@ -50,8 +52,6 @@ class Chestxray14Dataset(CustomDataset):
         Returns:
             list[dict]: Annotation info from COCO api.
         """
-        f = open(ann_file)
-        self.json_data = json.load(f)
         # self.coco = COCO(ann_file)
         # The order of returned `cat_ids` will not
         # change with the order of the CLASSES

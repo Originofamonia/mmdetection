2da98341e25503812fb0343aa5688cf1bcd9b499
Edward <anqiyuanms@hotmail.com>
2021-07-28 11:39:55 -0500

    'bboxes_ignore': np.zeros((0, 4), dtype=np.float32)}

--- mmdete/datasets/chestxray14.py
+++ mmdete/datasets/chestxray14.py
@@ -73,9 +73,10 @@ class Chestxray14Dataset(CustomDataset):
         img_id = self.data_infos[idx]['filename']
         for item in self.json_data:
             if item['file_name'] == img_id:
-                # return {'bboxes': np.asarray(item['boxes']),
-                #         'labels': [self.cat2index[sym] for sym in item['syms']]}
-                return self._parse_ann_info(self.data_infos[idx])
+                return {'bboxes': np.asarray(item['boxes']),
+                        'labels': [self.cat2index[sym] for sym in item['syms']],
+                        'bboxes_ignore': np.zeros((0, 4), dtype=np.float32)}
+                # return self._parse_ann_info(self.data_infos[idx])
 
         # return self._parse_ann_info(self.data_infos[idx], ann_info)
 

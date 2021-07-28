b12c6d628c20da0801f16fb3affa9f4841e26ca8
Edward <anqiyuanms@hotmail.com>
2021-07-28 12:01:51 -0500

    dict(type='RandomFlip', flip_ratio=None),

--- configs/cxr/chestxray14.py
+++ configs/cxr/chestxray14.py
@@ -73,7 +73,7 @@ train_pipeline = [
         min_ious=1,  # (0.4, 0.5, 0.6, 0.7, 0.8, 0.9),
         min_crop_size=0.3),
     dict(type='Resize', img_scale=[(320, 320), (608, 608)], keep_ratio=True),
-    # dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='RandomFlip', flip_ratio=None),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
